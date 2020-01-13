8ae1ebf543b3a53e1c9c9e3d71fbf5b35c4cb6be
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2020-01-13 11:37:25 +0100

    Initial Commit

---
+++ BarChartsAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartsAnimations
+//
+//  Created by De La Cruz, Eduardo on 13/01/2020.
+//  Copyright © 2020 De La Cruz, Eduardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
